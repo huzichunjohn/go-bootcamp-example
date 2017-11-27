6edb55dfab0805635877f93e079d220cbdfcb363
johnhu <huzichunjohn@126.com>
2017-11-27 16:56:59 +0800

    update rand.

--- src/rand.go
+++ src/rand.go
@@ -3,25 +3,35 @@ package main
 import (
     "fmt"
     "math/rand"
+    "time"
 )
 
 func main() {
-    newRandStream := func() <-chan int {
+    newRandStream := func(done <-chan interface{}) <-chan int {
 	randStream := make(chan int)
 	go func() {
 	    defer fmt.Println("newRandStream closure exited.")
 	    defer close(randStream)
 	    for {
-		randStream <- rand.Int()
+		select{
+		case randStream <- rand.Int():
+		case <-done:
+		    return
+		}
 	    }
 	}()
 
 	return randStream
     }
 
-    randStream := newRandStream()
+    done := make(chan interface{})
+    randStream := newRandStream(done)
     fmt.Println("3 random ints:")
     for i := 1; i <= 3; i++ {
 	fmt.Printf("%d: %d\n", i, <-randStream)
     }
+    close(done)
+
+    // Simulate ongoing work
+    time.Sleep(1*time.Second)
 }
