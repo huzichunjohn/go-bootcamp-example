5924956a1ff5e860e48e551491830226196b0691
johnhu <huzichunjohn@126.com>
2017-11-30 17:19:46 +0800

    add bridge.

--- src/generator.go
+++ src/generator.go
@@ -2,21 +2,20 @@ package main
 
 import (
     "fmt"
-    "math/rand"
-    "time"
-    "sync"
 )
 
 func main() {
-    repeatFn := func(done <-chan interface{}, fn func() interface{}) <-chan interface{} {
-	valueStream := make(chan interface{})
+    repeat := func(done <-chan interface{}, values ...int) <-chan int {
+	valueStream := make(chan int)
 	go func() {
 	    defer close(valueStream)
 	    for {
-		select {
-		case <-done:
-		    return
-		case valueStream <- fn():
+		for _, v := range values {
+		    select {
+		    case <-done:
+			return
+		    case valueStream <- v:
+		    }
 		}
 	    }
 	}()
@@ -38,94 +37,56 @@ func main() {
 	return takeStream
     }
 
-    toInt := func(done <-chan interface{}, valueStream <-chan interface{}) <-chan int {
-	intStream := make(chan int)
+    orDone := func(done, c <-chan interface{}) <-chan interface{} {
+	valStream := make(chan interface{})
 	go func() {
-	    defer close(intStream)
-	    for v := range valueStream {
-		select {
-		case <-done:
-		     return
-		case intStream <- v.(int):
-		}
-	    }
-	}()
-	return intStream
-    }
-
-    primeFinder := func(done <-chan interface{}, valueStream <-chan int) <-chan int {
-	intStream := make(chan int)
-	go func() {
-	    defer close(intStream)
-	    for v := range valueStream {
+	    defer close(valStream)
+	    for {
 		select {
 		case <-done:
 		    return
-		default:
-		    isPrime := true
-		    for i := 2; i < v; i++ {
-			if v%i == 0 {
-			    isPrime = false
-			    break
-			}
+		case v, ok := <-c:
+		    if ok == false {
+			return
 		    }
-		    if isPrime {
-			intStream <- v	
+		    select {
+		    case valStream <- v:
+		    case <-done:
 		    }
 		}
 	    }
 	}()
-	return intStream
+	return valStream
     }
 
-    fanIn := func(done <-chan interface{}, channels ...<-chan int) <-chan int {
-	var wg sync.WaitGroup
-	multiplexedStream := make(chan int)
-
-	multiplex := func(c <-chan int) {
-	    defer wg.Done()
-	    for i := range c {
-		select {
-		case <-done:
-		    return
-		case multiplexedStream <- i:
-		}
-	    }
-	}
-
-	wg.Add(len(channels))
-	for _, c := range channels {
-	    go multiplex(c)
-	}
-	
+    tee := func(done <-chan interface{}, in <-chan interface{}) (_, _ <-chan interface{}) {
+	out1 := make(chan interface{})
+	out2 := make(chan interface{})
 	go func() {
-	    wg.Wait()
-	    close(multiplexedStream)
-	}()	
-
-	return multiplexedStream
+	    defer close(out1)
+	    defer close(out2)
+	    for val := range orDone(done, in) {
+		var out1, out2 = out1, out2
+		for i := 0; i < 2; i++ {
+		    select {
+		    case <-done:
+		    case out1 <- val:
+		        out1 = nil
+		    case out2 <- val:
+		        out2 = nil
+		    }
+		}
+	    }	
+	}()
+	return out1, out2
     }
 
     done := make(chan interface{})
     defer close(done)
 
-    start := time.Now()
+    out1, out2 := tee(done, take(done, repeat(done, 1, 2), 4))
 
-    rand := func() interface{} { return rand.Intn(500000) }
-
-    randIntStream := toInt(done, repeatFn(done, rand))
-
-    numFinders := 16
-    fmt.Printf("Spinning up %d prime finders.\n", numFinders)
-    finders := make([]<-chan int, numFinders)
-    fmt.Println("Primes:")
-    for i := 0; i < numFinders; i++ {
-	finders[i] = primeFinder(done, randIntStream)
+    for val1 := range out1 {
+	fmt.Printf("out1: %v, out2: %v\n", val1, <-out2)
     }
-
-    for prime := range take(done, fanIn(done, finders...), 10) {
-	fmt.Printf("\t%d\n", prime)
-    }
-    
-    fmt.Printf("Search took: %v\n", time.Since(start))
 }
