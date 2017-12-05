7092475b4ea8392093540501fd8c0cf39dafff89
johnhu <huzichunjohn@126.com>
2017-12-05 16:29:49 +0800

    add test.

--- src/heartbeat.go
+++ src/heartbeat.go
@@ -2,72 +2,52 @@ package main
 
 import (
     "fmt"
-    "time"
+    "math/rand"
 )
 	
 func main() {
-    doWork := func(done <-chan interface{}, pulseInterval time.Duration) (<-chan interface{}, <-chan time.Time) {
-	heartbeat := make(chan interface{})
-	results := make(chan time.Time)
+    doWork := func(done <-chan interface{}) (<-chan interface{}, <-chan int) {
+	heartbeatStream := make(chan interface{}, 1)
+	workStream := make(chan int)
 	go func() {
-	    defer close(heartbeat)
-	    defer close(results)
+	    defer close(heartbeatStream)
+	    defer close(workStream)
 
-	    pulse := time.Tick(pulseInterval)
-	    workGen := time.Tick(2*pulseInterval)
-
-	    sendPulse := func() {
+	    for i := 0; i < 10; i++ {
 		select {
-		case heartbeat <- struct{}{}:
+		case heartbeatStream <- struct{}{}:
 		default:
 		}
-	    }
-	    sendResult := func(r time.Time) {
-		for {
-		    select {
-		    case <-done:
-			return
-		    case <-pulse:
-			sendPulse()
-		    case results <- r:
-			return
-		    }
-		}
-	    }
 
-	    for {
 		select {
 		case <-done:
 		    return
-		case <-pulse:
-		    sendPulse()
-		case r := <-workGen:
-		    sendResult(r)
+		case workStream <- rand.Intn(10):
 		}
 	    }
 	}()
-	return heartbeat, results
+
+	return heartbeatStream, workStream
     }
 
     done := make(chan interface{})
-    time.AfterFunc(10*time.Second, func() { close(done) })
+    defer close(done)
 
-    const timeout = 2*time.Second
-    heartbeat, results := doWork(done, timeout/2)
+    heartbeat, results := doWork(done)
     for {
 	select {
 	case _, ok := <-heartbeat:
-	    if ok == false {
-	        return
+	    if ok {
+		fmt.Println("pulse")
+	    } else {
+		return
 	    }
-	    fmt.Println("pulse")
 	case r, ok := <-results:
-	    if ok == false {
-	 	return
+	    if ok {
+		fmt.Printf("results %v\n", r)
+	    } else {
+		return
 	    }
-	    fmt.Printf("results %v\n", r.Second())
-	case <-time.After(timeout):
-	     return
-	}	
+	}
     }
 }
